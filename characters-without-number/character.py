a131037434973e64bd26fb804485c3316317ad92
Livingtnt88 <84382253+Livingtnt88@users.noreply.github.com>
2021-11-10 13:20:02 +1100

    items

--- characters-without-number/character.py
+++ characters-without-number/character.py
@@ -2,11 +2,11 @@ import random
 from __future__ import annotations
 
 class character:
-    def __init__(self, attributes, skills, background, Class):
+    def __init__(self, attributes, skills, background, Class, foci, items):
         pass
 
 class background:
-    def __init__(self, name, description, free_skill, quick_skills, growth, learning):
+    def __init__(self, name: str, description: str, free_skill: dict, quick_skills: dict, growth: list, learning: list):
         self.name = name
         self.decription = description
 
@@ -51,8 +51,57 @@ class Class:
             return False
 
 class foci:
-    def __init__(self, name, description, modifiers):
+    def __init__(self, name: str, description: str, modifiers, tags: list = None):
         self.name = name
         self.description = description
 
         self.modifiers = modifiers
+        self.tags = tags
+
+class item:
+    def __init__(self, name: str, description: str, cost: int, enc: int, tl: int = None, packable :bool = False) -> None:
+        self.name = name
+        self. description = description
+
+        self.cost = cost
+        self.enc = enc
+        self.packable = packable
+        self.tl = tl
+
+class armor(item):
+    def __init__(self, name: str, description: str, ac: int, cost: int, enc: int, tl: int = None, ac_bonus: int = 0) -> None:
+        self.ac = ac
+        self.ac_bonus = ac_bonus
+
+        super().__init__(name, description, cost, enc, tl)
+
+class weapon(item):
+    def __init__(self, name: str, description: str, damage: str, attribute: str, cost: int, enc: int, tl: int = None) -> None:
+        self.damage = damage
+        self.attribute = attribute
+        super().__init__(name, description, cost, enc, tl)
+
+class ranged(weapon):
+    def __init__(self, name: str, description: str, damage: str, attribute: str, range: str, magazine: int, ammo: item, cost: int, enc: int, tl: int = None, burst: bool = False, loading: bool = False) -> None:
+        self.range = range
+        self.magazine = magazine
+        self.ammo = ammo
+        self.burst = burst
+        self.loading = loading
+
+        super().__init__(name, description, damage, attribute, cost, enc, tl)
+
+class melee(weapon):
+    def __init__(self, name: str, description: str, damage: str, attribute: str, shock_ac: int, shock_dmg: int, cost: int, enc: int, tl: int = None) -> None:
+        self.shock_ac = shock_ac
+        self.shock_dmg = shock_dmg
+        super().__init__(name, description, damage, attribute, cost, enc, tl)
+
+class heavy(weapon):
+    def __init__(self, name: str, description: str, damage: str, attribute: str, range: str, magazine: int, ammo: item, cost: int, enc: int, tl: int = None, supression = False) -> None:
+        self.range = range
+        self.magazine = magazine
+        self.ammo = ammo
+        self.supression = supression
+
+        super().__init__(name, description, damage, attribute, cost, enc, tl=tl)
